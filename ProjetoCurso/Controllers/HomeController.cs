f1d7d584fc78fcb7e873771ad77122dad7015dc9
Leandro Zanatta <leandrozanatta7@gmail.com>
2021-07-22 15:42:58 -0300

    Testes iniciais

---
+++ ProjetoCurso/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoCurso.Models;
+using System.Diagnostics;
+
+namespace ProjetoCurso.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Criando uma aplicação MVC";
+            ViewData["Aluno"] = "Leandro Zanatta";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

94d0d080186bd9e064d623fe2a589a97d9d33947
Leandro Zanatta <Leandrozanatta7@gmail.com>
2021-08-13 22:07:08 -0300

    So para verificar

---
+++ ProjetoCurso/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjetoCurso.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}

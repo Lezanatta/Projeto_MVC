b72cb9cb442e05cfe07ede04ab8b4d0de8ae6986
Leandro Zanatta <Leandrozanatta7@gmail.com>
2021-08-16 11:09:20 -0300

    Formatacao das datas

--- ProjetoCurso/Startup.cs
+++ ProjetoCurso/Startup.cs
@@ -2,11 +2,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ProjetoCurso.Data;
 using ProjetoCurso.Services;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace ProjetoCurso
 {
@@ -44,6 +47,15 @@ namespace ProjetoCurso
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOption = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOption);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
